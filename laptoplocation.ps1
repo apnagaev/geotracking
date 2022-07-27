61eae712e08b7fecddb293c6ff4c5f6ded718d19
apnagaev <87721833+apnagaev@users.noreply.github.com>
2022-07-27 13:29:41 +0300

    Update laptoplocation.ps1

--- laptoplocation.ps1
+++ laptoplocation.ps1
@@ -1,6 +1,7 @@
 #############ChangeMe##################
 $server='geo.whereit.ru'
-$srvport=':45055'
+$srvport1=':45055'
+$srvport2=':8080'
 $srvproto='https'
 $yaapikey= Get-Content C:\scripts\key.txt
 ##################
@@ -86,7 +87,11 @@ if (($ac -eq $null) -or ($ac -eq 'True')) {$ac = 'AC'} else {$ac = 'Battery'}
 
 #http-get to geoserver
 if ($ipinf.zip -ne '') {$ipinf.zip = '&zip='+$ipinf.zip}
-$uri= $srvproto+'://'+$server+$srvport+'/?id='+$deviceid+'&timestamp='+$ts+'&lat='+$result.position.latitude+'&lon='+$result.position.longitude+'&realip='+$ipinf.query+'&batt='+$charge+'&isp='+$ipinf.isp+'&power='+$ac+'&accuracy='+$result.position.precision+'&computer_name='+$deviceid+'&username='+$username+$ipinf.zip
+$uri= $srvproto+'://'+$server+'/?id='+$deviceid+'&timestamp='+$ts+'&lat='+$result.position.latitude+'&lon='+$result.position.longitude+'&realip='+$ipinf.query+'&batt='+$charge+'&isp='+$ipinf.isp+'&power='+$ac+'&accuracy='+$result.position.precision+'&computer_name='+$deviceid+'&username='+$username+$ipinf.zip
+Invoke-WebRequest -Uri $uri
+$uri= $srvproto+'://'+$server+$srvport1+'/?id='+$deviceid+'&timestamp='+$ts+'&lat='+$result.position.latitude+'&lon='+$result.position.longitude+'&realip='+$ipinf.query+'&batt='+$charge+'&isp='+$ipinf.isp+'&power='+$ac+'&accuracy='+$result.position.precision+'&computer_name='+$deviceid+'&username='+$username+$ipinf.zip
+Invoke-WebRequest -Uri $uri
+$uri= $srvproto+'://'+$server+$srvport2+'/?id='+$deviceid+'&timestamp='+$ts+'&lat='+$result.position.latitude+'&lon='+$result.position.longitude+'&realip='+$ipinf.query+'&batt='+$charge+'&isp='+$ipinf.isp+'&power='+$ac+'&accuracy='+$result.position.precision+'&computer_name='+$deviceid+'&username='+$username+$ipinf.zip
 Invoke-WebRequest -Uri $uri
 
 
