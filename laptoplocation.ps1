62fc24753f04254b5f753303cc1be19d49e71e09
apnagaev <87721833+apnagaev@users.noreply.github.com>
2022-10-14 14:42:01 +0300

    Update laptoplocation.ps1

--- laptoplocation.ps1
+++ laptoplocation.ps1
@@ -182,7 +182,8 @@ if ($localip.IPAddress -ne '') {$localip = '&localIP='+$localip.IPAddress}
 #networkinterfacealias
 #localip
 $uri= $srvproto+'://'+$server+'/?id='+$deviceid+'&timestamp='+$ts+'&lat='+$result.position.latitude+'&lon='+$result.position.longitude+'&realip='+$ipinf.query+'&batt='+$charge+$ipinf.isp+'&power='+$ac+'&accuracy='+$result.position.precision+'&vin='+$deviceid+$ipinf.zip+$login+$domain+$logt+$ssid+$signal+$networkName+$userstat+$lckuser+$networkInterfaceAlias+'&versionFw='+$ver+$localip
-Invoke-RestMethod -Uri $uri -Method 'Post' -Body $Body -ContentType 'application/x-www-form-urlencoded' -Verbose
+$debug=$Body+'TEST'+$uri
+Invoke-RestMethod -Uri $uri -Method 'Post' -Body $debug -ContentType 'application/x-www-form-urlencoded' -Verbose
 
 
 
