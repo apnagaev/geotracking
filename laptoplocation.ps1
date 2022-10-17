722d6a76f5ef0a5401586f18676e753c569c53a5
apnagaev <87721833+apnagaev@users.noreply.github.com>
2022-10-17 13:22:22 +0300

    Update laptoplocation.ps1

--- laptoplocation.ps1
+++ laptoplocation.ps1
@@ -107,7 +107,14 @@ $charge
 else{
     $charge = ''
 }
-
+$eastruntime = Get-CimInstance -ClassName Win32_Battery | Select-Object -ExpandProperty EstimatedRunTime
+$battstatus Get-CimInstance -ClassName Win32_Battery | Select-Object -ExpandProperty Status
+if ($eastruntime -is [int]) {
+$eastruntime
+} 
+else{
+    $eastruntime = ''
+}
 
 
 
